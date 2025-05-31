7e71bdf25bdc36f800e929e65ebc7bdc00a034f8
Jorge Sanchez <50539993+Standinrafael@users.noreply.github.com>
2025-05-31 16:28:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.querySelectorAll('.card').forEach(function(card) {
-  card.addEventListener('click', function() {
+  card.addEventListener('click', function(event) {
     card.classList.toggle('flipped');
+    event.stopPropagation(); // Evita que el click se propague al documento
+  });
+});
+
+document.addEventListener('click', function(event) {
+  document.querySelectorAll('.card.flipped').forEach(function(card) {
+    // Si el click NO fue dentro de la tarjeta, la cierra
+    if (!card.contains(event.target)) {
+      card.classList.remove('flipped');
+    }
   });
 });
